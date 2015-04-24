d4c350744ac43832814c0a3763126cd4364be867
willcore <corlett.william@gmail.com>
2015-04-24 07:56:05 -0400

    Update to Rays

--- configVars.py
+++ configVars.py
@@ -1,3 +1,5 @@
+
+
 __author__ = 'William'
 
 import RPi.GPIO as GPIO
@@ -11,27 +13,41 @@ milimeterMode = True
 dummySTEP = 20
 dummyDIR = 21
 
-xSTEP = 20
-xDIR = 21
+xSTEP = 17
+xDIR = 27
 
-ySTEP = 12 
-yDIR = 16
+ySTEP = 22 
+yDIR = 5
 
-zSTEP = 19 
-zDIR = 26
+zSTEP = 6 
+zDIR = 13
 
-eSTEP =27 
-eDIR = 22
+eSTEP = 19 
+eDIR = 26
 
-xStopper = 13 
-yStopper = 6
+xStopper = 18 
+yStopper = 23
+zStopper = 24
 
+#number of steps per mm (calibration)
+xSteps = 109.34
+ySteps = 114.68
+zSteps = 1986.18
+eSteps = 101.23
 
 filamentDiameter = 0.0
 
-feedrate = 0.0
+#in mm/s
+feedrate = 100.0
 
-temperature = 0.0
+#these are in mm/s
+xMaxFeedrate = 100.0
+yMaxFeedrate = 100.0
+zMaxFeedrate = 100.0
+eMaxFeedrate = 100.0
+
+targetTemp = 0
+curTemp = 0
 
 fanRate = 0.0
 
